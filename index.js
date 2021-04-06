8b25a9135b97ea9453d7e529c6e8ae229c938f57
Sukaina Charaf Eddine <sukaina.kabalan01@gmail.com>
2021-04-06 10:22:41 +0000

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,23 @@
+<script>
+var num1=31;
+var num2=2;
+var multiply=num1*num2;
 
-// Write your code here
+
+var=random;
+Math.floor(Math.random() *Infinity) +0;
+
+
+var num3=100;
+var num4=6;
+var mod=num3%num4;
+
+var max=Math.max(1,15,20,6,7)
+
+
+
+
+
+
+
+</script>
